f161028c0ba064121b2827acd40a039d94d351eb
Ian Ross <105084829+CoBalt999@users.noreply.github.com>
2025-05-15 15:48:21 -0400

    added human and computer choice; started on round code

--- scripts.js
+++ scripts.js
@@ -1,18 +1,44 @@
-let choice = 'none'
+let computerChoice = 'none'
+let humanChoice = `none`
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let randomNumber = Math.random()
     
     if (randomNumber <= 0.3) {
-        choice = "Rock"}
+        computerChoice = "Rock"}
     else if (randomNumber <= 0.6) {
-        choice = "Paper"
+        computerChoice = "Paper"
     }
 
     else {
-        choice = "Scissors"
+        computerChoice = "Scissors"
     }
     
-    return choice
+    return computerChoice
 };
 
-getComputerChoice()
+function getHumanChoice() {
+    humanChoice = prompt("Choice: Rock, Paper, or Scissors")
+    return humanChoice
+};
+
+function playRound(humanChoice,computerChoice) {
+if (arguments[0] == arguments[1]) {
+    return `It's a tie!`}
+else if (arguments[0] = `Rock` && arguments[1] != `Paper`) {
+    return `Player Wins!`
+}
+else if (arguments[0] = `Paper` && arguments[1] != `Scissors`) {
+    return `Player Wins!`
+}
+else if (arguments[0] = `Scissors` && arguments[1] != `Rock`) {
+    return `Player Wins!`
+}
+else if 
+};
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
